aa0f3f8e3530f2b7f51d548f7d287ef905796e0b
Alejandro-Rodriguez-Santos <alejandro.rodriguez38@alu.uclm.es>
2024-11-27 17:34:47 +0100

    Creacion de proyecto

---
+++ src/ejercicio01/src/test/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/AppTest.java
@@ -0,0 +1,19 @@
+package java.es.UCLM.esi.ISO2.C01.ejercicio01;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
