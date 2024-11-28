7fa192b5384424a9f1e6b0b0aaa62906d905c0f1
Alejandro-Rodriguez-Santos <alejandro.rodriguez38@alu.uclm.es>
2024-11-28 23:31:26 +0100

    Modificacion de nivel a tipo TITULACION, para posterior uso

--- src/ejercicio01/src/main/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/Persona.java
+++ src/ejercicio01/src/main/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/Persona.java
@@ -1,25 +1,40 @@
 package java.es.UCLM.esi.ISO2.C01.ejercicio01;
 
+import java.io.IOException;
 import java.time.LocalDate;
-public class Persona implements IVariables{
+
+public class Persona implements IVariables {
+
 	private String _nombre;
 	private String _apellidos;
 	private LocalDate _fecha_nacimiento;
 	private Pais _nacionalidad;
 	private Matricula _matricula;
-	private enum _certificacion{
-	}
-	
-	// para luego comprobar nivel de titulacion. igual al final no es necesario 
-	private final int nivel;
 
-	// Constructor
-	Persona(int nivel) {
-	    this.nivel = nivel;
+	private enum _certificacion {
 	}
+
+	// para luego comprobar nivel de titulacion. igual al final no es necesario
+	
+	/*
+	 * pongo nivel como tipo titulacion en lugar de como int
+	 */
+	private final TITULACION nivel;
 	private int _numero_tlf;
 	private String _correo_electronico;
 
+	
+	public Persona(String _nombre, String _apellidos, LocalDate _fecha_nacimiento, Pais _nacionalidad,
+			Matricula _matricula, TITULACION nivel, int _numero_tlf, String _correo_electronico) {
+		this._nombre = _nombre;
+		this._apellidos = _apellidos;
+		this._fecha_nacimiento = _fecha_nacimiento;
+		this._nacionalidad = _nacionalidad;
+		this._matricula = _matricula;
+		this.nivel = setnivel(nivel);
+		this._numero_tlf = _numero_tlf;
+		this._correo_electronico = _correo_electronico;
+	}
 
 	public void esEuropeo() {
 		throw new UnsupportedOperationException();
@@ -29,8 +44,92 @@ public class Persona implements IVariables{
 		throw new UnsupportedOperationException();
 	}
 
-	public void HacerMatricula() {
-// metodo con lo de las variables de en una interfaz con enum de titulacion y su nivel de prioridad
-		throw new UnsupportedOperationException();
+	/*public void HacerMatricula() throws Exception {
+		try {
+			if (nivel >= TITULACION.MASTER.ordinal() + 1) {
+				_matricula = new Matricula(_apellidos, _fecha_nacimiento);
+				// System.out.println("Matrícula realizada con éxito.");
+				return;
+			}
+			System.out.println("No se alcanza la titulacion requerida");
+		}
+
+		catch (Exception e) {
+			System.out.println("Ha ocurrido un problema:\n" + e);
+		}
+
+	}*/
+
+	public String get_nombre() {
+		return _nombre;
 	}
+
+	public void set_nombre(String _nombre) {
+		try {
+			
+			
+			this._nombre = _nombre;
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	public String get_apellidos() {
+		return _apellidos;
+	}
+
+	public void set_apellidos(String _apellidos) {
+		this._apellidos = _apellidos;
+	}
+
+	public LocalDate get_fecha_nacimiento() {
+		return _fecha_nacimiento;
+	}
+
+	public void set_fecha_nacimiento(LocalDate _fecha_nacimiento) {
+		this._fecha_nacimiento = _fecha_nacimiento;
+	}
+
+	public Pais get_nacionalidad() {
+		return _nacionalidad;
+	}
+
+	public void set_nacionalidad(Pais _nacionalidad) {
+		this._nacionalidad = _nacionalidad;
+	}
+
+	public Matricula get_matricula() {
+		return _matricula;
+	}
+
+	public void set_matricula(Matricula _matricula) {
+		this._matricula = _matricula;
+	}
+
+	public int get_numero_tlf() {
+		return _numero_tlf;
+	}
+
+	public void set_numero_tlf(int _numero_tlf) {
+		this._numero_tlf = _numero_tlf;
+	}
+
+	public String get_correo_electronico() {
+		return _correo_electronico;
+	}
+
+	public void set_correo_electronico(String _correo_electronico) {
+		this._correo_electronico = _correo_electronico;
+	}
+
+	public TITULACION getNivel() {
+
+		return nivel;
+	}
+	public TITULACION setnivel (TITULACION nivel) {
+		
+		return nivel;
+	}
+	
 }
