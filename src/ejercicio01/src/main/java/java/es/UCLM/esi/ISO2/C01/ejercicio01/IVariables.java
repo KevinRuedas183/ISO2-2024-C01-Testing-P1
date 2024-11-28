364327aef57c23cf82354a41695c901ef3019481
Alejandro-Rodriguez-Santos <alejandro.rodriguez38@alu.uclm.es>
2024-11-28 21:21:24 +0100

    Creacion de interfaz para guardar variables
    En interfaz tenemos enum, para guardar las titulaciones posibles con su
    nivel de prioridad correspondiente para luego en el metodo hacer
    matricula hacer las comprobaciones para poder o no hacer matricula

---
+++ src/ejercicio01/src/main/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/IVariables.java
@@ -0,0 +1,16 @@
+package java.es.UCLM.esi.ISO2.C01.ejercicio01;
+
+public interface IVariables {
+
+
+	public enum TITULACION { 
+		LICENCIATURA(1),
+		MASTER(2),
+		DOCTORADO(3);
+
+		TITULACION(int i) {
+			// TODO Auto-generated constructor stub
+		}
+			
+		}
+}
--- src/ejercicio01/src/main/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/Persona.java
+++ src/ejercicio01/src/main/java/java/es/UCLM/esi/ISO2/C01/ejercicio01/Persona.java
@@ -1,15 +1,21 @@
 package java.es.UCLM.esi.ISO2.C01.ejercicio01;
 
 import java.time.LocalDate;
-public class Persona {
+public class Persona implements IVariables{
 	private String _nombre;
 	private String _apellidos;
 	private LocalDate _fecha_nacimiento;
 	private Pais _nacionalidad;
 	private Matricula _matricula;
 	private enum _certificacion{
-		
-		
+	}
+	
+	// para luego comprobar nivel de titulacion. igual al final no es necesario 
+	private final int nivel;
+
+	// Constructor
+	Persona(int nivel) {
+	    this.nivel = nivel;
 	}
 	private int _numero_tlf;
 	private String _correo_electronico;
@@ -24,7 +30,7 @@ public class Persona {
 	}
 
 	public void HacerMatricula() {
-
+// metodo con lo de las variables de en una interfaz con enum de titulacion y su nivel de prioridad
 		throw new UnsupportedOperationException();
 	}
 }
